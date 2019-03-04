df47c58fb77bdb608822cf297835137dc6ec34f4
SilkFlowers <44918641+SilkFlowers@users.noreply.github.com>
2019-03-03 16:36:18 -0800

    Add files via upload

---
+++ simpcalc/simpcalc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simpcalc
+//
+//  Created by josh park on 3/3/19.
+//  Copyright © 2019 joshuapark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
